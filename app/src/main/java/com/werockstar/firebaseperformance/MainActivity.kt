ad506e6ecae20057dd5020b68e6e7a522ce23283
WeRockStar <kotchaphan.m@gmail.com>
2017-05-23 15:53:37 +0700

    Initial commit

---
+++ app/src/main/java/com/werockstar/firebaseperformance/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.werockstar.firebaseperformance
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
